125bb83d728d6697b58541beb920ad58b0c03cf5
Lucas Corcodilos <lcorcodi@cern.ch>
2020-07-22 15:16:36 -0500

    Move CMS interfacing helpers to bottom (for readability)

--- fastsimTrackingHelpers.py
+++ fastsimTrackingHelpers.py
@@ -6,53 +6,6 @@ from contextlib import contextmanager
 from collections import OrderedDict
 from fastsimTrackingValidation import *
 
-#------------CMS interfacing-----------------------------------------#
-def MakeCrabConfig(stepname, tag, files=[],storageSite='T3_US_FNALLPC'):
-    # if stepname in ['AOD','BTAGVAL','MINIAOD','NANOAOD']:
-    gen = True
-    ana = False
-    # else:
-    #     gen = False
-    #     ana = True
-
-    if files != [] and gen:
-        raise ValueError("You are trying to generate but the inputFiles list is non-empty. Perhaps you meant to run an analysis script?")
-    if files == [] and ana:
-        raise ValueError("You are trying to analyze but the inputFiles list is empty.")
-
-    crab_config = config()
-    crab_config.General.requestName = 'FastSimValidation_%s'%stepname
-    crab_config.General.workArea = stepname
-    crab_config.General.transferOutputs = True
-    crab_config.JobType.psetName = 'FastSimValidation_%s.py'%stepname
-    crab_config.Data.publication = False
-    crab_config.Site.storageSite = storageSite
-    crab_config.Data.outLFNDirBase = '/store/user/lcorcodi/FastSimValidation/'
-    crab_config.Data.outputDatasetTag = tag
-
-    # if gen:
-    crab_config.JobType.pluginName = 'PrivateMC'
-    crab_config.Data.splitting = 'EventBased'
-    crab_config.Data.unitsPerJob = 1000
-    # elif ana:
-    #     crab_config.JobType.pluginName = 'Analysis'
-    #     crab_config.Data.inputFiles = files
-    #     crab_config.Data.splitting = 'FileBased'
-    #     crab_config.Data.unitsPerJob = 1
-
-    return crab_config
-
-def MakeRunConfig(inputArgs):
-    remove_spaces = []
-    for a in inputArgs:
-        s = a.split(' ')
-        for p in s:
-            if p != '': remove_spaces.append(p)
-    driver_args = ['--no_exec']
-    driver_args.extend(remove_spaces)
-    print (driver_args)
-    cmsDriverAPI.run(driver_args)
-
 #--------------------Helpers-----------------------------------------#
 def ParseSteps(allBool,steps):
     step_list = steps.split(',')
@@ -141,3 +94,50 @@ def cd(newdir):
         yield
     finally:
         os.chdir(prevdir)
+
+#------------CMS interfacing-----------------------------------------#
+def MakeCrabConfig(stepname, tag, files=[],storageSite='T3_US_FNALLPC'):
+    # if stepname in ['AOD','BTAGVAL','MINIAOD','NANOAOD']:
+    gen = True
+    ana = False
+    # else:
+    #     gen = False
+    #     ana = True
+
+    if files != [] and gen:
+        raise ValueError("You are trying to generate but the inputFiles list is non-empty. Perhaps you meant to run an analysis script?")
+    if files == [] and ana:
+        raise ValueError("You are trying to analyze but the inputFiles list is empty.")
+
+    crab_config = config()
+    crab_config.General.requestName = 'FastSimValidation_%s'%stepname
+    crab_config.General.workArea = stepname
+    crab_config.General.transferOutputs = True
+    crab_config.JobType.psetName = 'FastSimValidation_%s.py'%stepname
+    crab_config.Data.publication = False
+    crab_config.Site.storageSite = storageSite
+    crab_config.Data.outLFNDirBase = '/store/user/lcorcodi/FastSimValidation/'
+    crab_config.Data.outputDatasetTag = tag
+
+    # if gen:
+    crab_config.JobType.pluginName = 'PrivateMC'
+    crab_config.Data.splitting = 'EventBased'
+    crab_config.Data.unitsPerJob = 1000
+    # elif ana:
+    #     crab_config.JobType.pluginName = 'Analysis'
+    #     crab_config.Data.inputFiles = files
+    #     crab_config.Data.splitting = 'FileBased'
+    #     crab_config.Data.unitsPerJob = 1
+
+    return crab_config
+
+def MakeRunConfig(inputArgs):
+    remove_spaces = []
+    for a in inputArgs:
+        s = a.split(' ')
+        for p in s:
+            if p != '': remove_spaces.append(p)
+    driver_args = ['--no_exec']
+    driver_args.extend(remove_spaces)
+    print (driver_args)
+    cmsDriverAPI.run(driver_args)
