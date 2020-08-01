1036c8e1d24910aa8c768c9d87b579df17befa3a
Lucas Corcodilos <lcorcodi@cern.ch>
2020-07-31 23:45:58 -0500

    Working compare.py (trackval, btagval)

--- compare.py
+++ compare.py
@@ -1,4 +1,4 @@
-import subprocess, os
+import subprocess, os, ROOT
 from common import fastsimTrackingHelpers as helper
 from optparse import OptionParser
 
@@ -17,30 +17,69 @@ parser.add_option('-n', metavar='F', type='string', action='store',
                 dest    =   'names',
                 help    =   'Comma separated list of dir names in same order as dirs option.')                
 parser.add_option('-s',"--steps", type='string', action="store", 
-                default =   '',
+                default =   'ALL',
                 dest    =   "steps",
                 help    =   "Comma separated list of steps. Possible are TRACKVAL,BTAGVAL,ANALYSIS,ALL.")
+(options, args) = parser.parse_args()
+
+cwd = os.getcwd()
 
 def BtagValPlotting(valdirs,names):
     if len(valdirs) > 2:
         print 'WARNING: BTAGVAL can only compare two sets at a time. Only the first two listed will be considered.'
 
-    plotFactory_cmd = 'python Validation-Tools/plotProducer/scripts/plotFactory.py -f %s/FastSimValWorkspace/BTAGVAL/harvest.root -F %s/FastSimValWorkspace/BTAGVAL/harvest.root -r %s -R %s -s TTbar_13TeV_TuneCUETP8M1 -S TTbar_13TeV_TuneCUETP8M1_cfi'%(valdirs[0],valdirs[1],names[0],names[1])
-    header.executeCmd(plotFactory_cmd,bkg=True)
+    first = valdirs[0] if '.root' in valdirs[0] else valdirs[0]+'/FastSimValWorkspace/BTAGVAL/harvest.root'
+    second = valdirs[1] if '.root' in valdirs[1] else valdirs[1]+'/FastSimValWorkspace/BTAGVAL/harvest.root'
+
+    with helper.cd('btag_plots/'):
+        plotFactory_cmd = 'python %s/Validation-Tools/plotProducer/scripts/plotFactory.py -f ../../%s -F ../../%s -r %s -R %s -s TTbar_13TeV_TuneCUETP8M1 -S TTbar_13TeV_TuneCUETP8M1_cfi'%(cwd,first,second,names[0],names[1])
+        helper.executeCmd(plotFactory_cmd,bkg=False)
 
 def TrackValPlotting(valdirs):
     trackval_cmd = 'makeTrackValidationPlots.py -o tracking_plots'
     for d in valdirs:
-        trackval_cmd+=' %s/FastSimValWorkspace/TRACKVAL/harvestTracks.root'%(d)
-    header.executeCmd(plotFactory_cmd,bkg=True)
+        if '.root' in d:
+            trackval_cmd+=' ../%s'%(d)
+        else:
+            trackval_cmd+=' ../%s/FastSimValWorkspace/TRACKVAL/harvestTracks.root'%(d)
+    helper.executeCmd(trackval_cmd,bkg=False)
 
-def AnalysisPlotting():
-    pass
+def AnalysisPlotting(valdirs,names):
+    if len(valdirs) > 2:
+        print 'WARNING: ANALYSIS can only compare up to four sets at a time. Only the first four listed will be considered.'
 
-(options, args) = parser.parse_args()
+    canvases = {}
+    legends = {}
+    colors = [ROOT.kRed,ROOT.Blue,ROOT.kGreen,ROOT.kOrange]
+    for i,d in enumerate(valdirs):
+        if '.root' in d:
+            f = ROOT.TFile.Open(d)
+        else:
+            f = ROOT.TFile.Open('../%s/FastSimValWorkspace/ANALYSIS/FastSim_ANALYSIS.root'%d)
+        for key in f.GetListOfKeys():
+            hname = key.GetName()
+            if hname not in canvases.keys():
+                canvases[hname] = ROOT.TCanvas(hname,hname,800,700)
+                legends[hname] = ROOT.TLegend(0.75,0.75,0.95,0.95)
+            
+            h = f.Get(key.GetName())
+            canvases[hname].cd()
+            h.SetDirectory(0)
+            h.SetLineColor(colors[i])
+            legends[hname].AddEntry(h,names[i],'l')
+            h.Draw('same hist e')
+    
+    for ckey in canvases.keys():
+        canvases[ckey].Print('AnalysisVars/%s.pdf'%ckey,'pdf')
+
+def optionsCheck(options):
+    if options.tag =='':
+        raise ValueError('Option `tag` must be specified. This determines directory where plots will be saved.')
 
 #------------------------------------------------------------#
 if __name__ == '__main__':
+    optionsCheck(options)
+
     # Grab comparison info
     valdirs = options.dirs.split(',')
     if options.names == '':
@@ -54,11 +93,14 @@ if __name__ == '__main__':
     if not os.path.isdir(options.tag):
         print('mkdir '+options.tag)
         os.mkdir(options.tag)
+
     # Change dir and do each step
-    with header.cd(options.tag):
+    with helper.cd(options.tag):
         if 'ALL' in options.steps or 'BTAGVAL' in options.steps:
             BtagValPlotting(valdirs,names)
         if 'ALL' in options.steps or 'TRACKVAL' in options.steps:
             TrackValPlotting(valdirs)
         if 'ALL' in options.steps or 'ANALYSIS' in options.steps:
-            AnalysisPlotting()
+            if not os.path.exists('AnalysisVars/'):
+                os.mkdir('AnalysisVars')
+            AnalysisPlotting(valdirs,name)
