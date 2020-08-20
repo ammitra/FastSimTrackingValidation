86583ce684aa7eafa0351fff6cc3a8707ef02601
Lucas Corcodilos <lcorcodi@cern.ch>
2020-08-20 10:32:21 -0500

    Small fixes to compare.py

--- compare.py
+++ compare.py
@@ -28,27 +28,27 @@ cwd = os.getcwd()
 
 def BtagValPlotting(valdirs,names):
     if len(valdirs) > 2:
-        print 'WARNING: BTAGVAL can only compare two sets at a time. Only the first two listed will be considered.'
+        print ('WARNING: BTAGVAL can only compare two sets at a time. Only the first two listed will be considered.')
 
     first = valdirs[0] if '.root' in valdirs[0] else valdirs[0]+'/FastSimValWorkspace/BTAGVAL/harvest.root'
     second = valdirs[1] if '.root' in valdirs[1] else valdirs[1]+'/FastSimValWorkspace/BTAGVAL/harvest.root'
 
     with helper.cd('btag_plots/'):
-        plotFactory_cmd = 'python %s/Validation-Tools/plotProducer/scripts/plotFactory.py -f ../../%s -F ../../%s -r %s -R %s -s TTbar_13TeV_TuneCUETP8M1 -S TTbar_13TeV_TuneCUETP8M1_cfi'%(cwd,first,second,names[0],names[1])
+        plotFactory_cmd = 'python %s/Validation-Tools/plotProducer/scripts/plotFactory.py -f ../../%s -F ../../%s -r %s -R %s -s TTbar_13TeV_TuneCUETP8M1 -S TTbar_13TeV_TuneCUETP8M1'%(cwd,first,second,names[0],names[1])
         helper.executeCmd(plotFactory_cmd,bkg=False)
 
 def TrackValPlotting(valdirs):
     trackval_cmd = 'makeTrackValidationPlots.py -o tracking_plots'
     for d in valdirs:
         if '.root' in d:
-            trackval_cmd+=' %s'%(d)
+            trackval_cmd+=' ../%s'%(d)
         else:
             trackval_cmd+=' ../%s/FastSimValWorkspace/TRACKVAL/harvestTracks.root'%(d)
     helper.executeCmd(trackval_cmd,bkg=False)
 
 def AnalysisPlotting(valdirs,names):
     if len(valdirs) > 2:
-        print 'WARNING: ANALYSIS can only compare up to four sets at a time. Only the first four listed will be considered.'
+        print ('WARNING: ANALYSIS can only compare up to four sets at a time. Only the first four listed will be considered.')
 
     canvases = {}
     legends = {}
